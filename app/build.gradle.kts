277ca5ef5c23db60bd88c5c42fe5dddc6faf5c67
hercullezz <hercules.daluz@hotmail.com>
2026-04-16 12:11:33 -0400

    chore: bump version to 1.8 with new professional icon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         minSdk = 26
         targetSdk = 36
         // Atualizando versão para permitir update "por cima" sem perder dados
-        versionCode = 8
-        versionName = "1.7"
+        versionCode = 9
+        versionName = "1.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
